b93dc02648e3768607f6f04f0eab92b2ec37c79e
Carlos Agullo <carlos.agullo@7shifts.com>
2020-04-17 04:57:58 -0400

    Working on the lasts test for API (Picture)

--- src/__tests__/api.test.js
+++ src/__tests__/api.test.js
@@ -11,7 +11,7 @@ import {
 } from '../constants';
 
 describe('API tests', () => {
-    describe('Get all games', () => {
+    describe('Get all Games', () => {
         it('Returns empty array', () => {
             jest.spyOn(axios, 'get').mockImplementation(
                 () => Promise.resolve(successResponse([]))
@@ -22,7 +22,7 @@ describe('API tests', () => {
                 expect(axios.get).toHaveBeenCalledWith(games_url());
             });
         });
-        it('Returns one result', () => {
+        it('Returns only one Game', () => {
             const mockedGame = {"game_id":1,"city":"Tokyo","year":2020};
             jest.spyOn(axios, 'get').mockImplementation(
                 () => Promise.resolve(successResponse([mockedGame]))
@@ -33,7 +33,7 @@ describe('API tests', () => {
                 expect(axios.get).toHaveBeenCalledWith(games_url());
             });
         });
-        it('Returns array of results one result', () => {
+        it('Returns array of Games', () => {
             const mockedArrayGame = [
                 {"game_id":1,"city":"Tokyo","year":2020},
                 {"game_id":2,"city":"Rio de Janeiro","year":2016},
@@ -49,7 +49,7 @@ describe('API tests', () => {
                 expect(axios.get).toHaveBeenCalledWith(games_url());
             });
         });
-        it('Arroja un error', () => {
+        it('Returns error and promise is rejected', () => {
             const errorMsg = 'default.error_occurred';
             jest.spyOn(axios, 'get').mockImplementation(
                 () => Promise.reject(errorMsg)
@@ -59,6 +59,219 @@ describe('API tests', () => {
                 expect(e).toBeInstanceOf(Error);
                 expect(e).toStrictEqual(Error(errorMsg));
                 expect(axios.get).toHaveBeenCalledWith(games_url());
+            });
+        });
+    });
+    describe('Get Athletes with Medals in each Game', () => {
+        const game_id = 3;
+        it('Returns no results for game id', () => {
+            jest.spyOn(axios, 'get').mockImplementation(
+                () => Promise.resolve(successResponse([]))
+            );
+
+            return api.getAthletesByGame(game_id).then(response => {
+                expect(response.data).toStrictEqual([]);
+                expect(axios.get).toHaveBeenCalledWith(
+                    athletes_by_game_url(game_id)
+                );
+            });
+        });
+        it('Returns one athlete with medals', () => {
+            const mocked_athlete = {
+                athlete_id: 11,
+                bio: "**Hello**",
+                dateOfBirth: '01-01-2000',
+                height: 190,
+                name: "Mike",
+                photo_id: 11,
+                surname: "Mousse",
+                weight: 90
+            };
+        
+            jest.spyOn(axios, 'get').mockImplementation(
+                () => Promise.resolve(successResponse([mocked_athlete]))
+            );
+
+            return api.listGames().then(response => {
+                expect(response.data).toStrictEqual([mocked_athlete]);
+                expect(axios.get).toHaveBeenCalledWith(
+                    athletes_by_game_url(game_id)
+                );
+            });
+        });
+        it('Returns various athletes for the Games', () => {
+            const mocked_athletes = [
+                {
+                    athlete_id: 11,
+                    bio: "**Hello**",
+                    dateOfBirth: '01-01-2000',
+                    height: 190,
+                    name: "Mike",
+                    photo_id: 11,
+                    surname: "Mousse",
+                    weight: 90
+                },
+                {
+                    athlete_id: 12,
+                    bio: "**Bye**",
+                    dateOfBirth: '01-01-1998',
+                    height: 178,
+                    name: "Mary",
+                    photo_id: 12,
+                    surname: "Pink",
+                    weight: 70
+                }
+            ];
+
+            jest.spyOn(axios, 'get').mockImplementation(
+                () => Promise.resolve(successResponse(mocked_athletes))
+            );
+
+            return api.listGames().then(response => {
+                expect(response.data).toStrictEqual(mocked_athletes);
+                expect(axios.get).toHaveBeenCalledWith(
+                    athletes_by_game_url(game_id)
+                );
+            });
+        });
+        it('Returns an error', () => {
+            const errorMsg = 'default.error_occurred';
+            jest.spyOn(axios, 'get').mockImplementation(
+                () => Promise.reject(errorMsg)
+            );
+
+            return api.listGames().catch(e => {
+                expect(e).toBeInstanceOf(Error);
+                expect(e).toStrictEqual(Error(errorMsg));
+                expect(axios.get).toHaveBeenCalledWith(
+                    athletes_by_game_url(game_id)
+                );
+            });
+        });
+    });
+    describe('Get Athlete Results Info', () => {
+        const athlete_id = 25;
+        it('Returns no results for athlete', () => {
+            jest.spyOn(axios, 'get').mockImplementation(
+                () => Promise.resolve(successResponse([]))
+            );
+
+            return api.getAthleteResultsInfo(athlete_id).then(response => {
+                expect(response.data).toStrictEqual([]);
+                expect(axios.get).toHaveBeenCalledWith(
+                    athlete_results_url(athlete_id)
+                );
+            });
+        });
+        it('Returns one athlete result', () => {
+            const mocked_game_results = {
+                bronze: 1,
+                city: 'Barcelona',
+                fourths: 3,
+                gold: 1,
+                silver: 1,
+                year: 1992
+            };
+        
+            jest.spyOn(axios, 'get').mockImplementation(
+                () => Promise.resolve(successResponse([mocked_game_results]))
+            );
+
+            return api.listGames().then(response => {
+                expect(response.data).toStrictEqual([mocked_game_results]);
+                expect(axios.get).toHaveBeenCalledWith(
+                    athlete_results_url(athlete_id)
+                );
+            });
+        });
+        it('Returns results for various Games', () => {
+            const mocked_game_results = [
+                {
+                    bronze: 1,
+                    city: 'Barcelona',
+                    fourths: 3,
+                    gold: 1,
+                    silver: 1,
+                    year: 1992
+                }, 
+                {
+                    bronze: 0,
+                    city: 'South Korea',
+                    fourths: 2,
+                    gold: 0,
+                    silver: 2,
+                    year: 1992
+                }
+            ];
+
+            jest.spyOn(axios, 'get').mockImplementation(
+                () => Promise.resolve(successResponse(mocked_game_results))
+            );
+
+            return api.listGames().then(response => {
+                expect(response.data).toStrictEqual(mocked_game_results);
+                expect(axios.get).toHaveBeenCalledWith(
+                    athlete_results_url(athlete_id)
+                );
+            });
+        });
+        it('Returns an error', () => {
+            const errorMsg = 'default.error_occurred';
+            jest.spyOn(axios, 'get').mockImplementation(
+                () => Promise.reject(errorMsg)
+            );
+
+            return api.listGames().catch(e => {
+                expect(e).toBeInstanceOf(Error);
+                expect(e).toStrictEqual(Error(errorMsg));
+                expect(axios.get).toHaveBeenCalledWith(
+                    athlete_results_url(athlete_id)
+                );
+            });
+        });
+    });
+    describe('Get Athletes Photos', () => {
+        const photo_id = 15;
+        it('Returns no results for photo id', () => {
+            jest.spyOn(axios, 'get').mockImplementation(
+                () => Promise.resolve(successResponse(''))
+            );
+
+            return api.getAthletePicture(photo_id).then(response => {
+                expect(response).toStrictEqual('');
+                // expect(axios.get).toHaveBeenCalledWith(
+                //     athlete_photo_url(photo_id)
+                // );
+            });
+        });
+        it('Returns one thumbnail', () => {
+            const photo_id = 15;
+            const string_photo = '/9j/4AAQSkZJRgABAQAAAQABAAD/4gxYSUNDX1BS';
+        
+            jest.spyOn(axios, 'get').mockImplementation(
+                () => Promise.resolve(successResponse(ArrayBuffer))
+            );
+
+            return api.getAthletePicture(photo_id).then(response => {
+                expect(response).toStrictEqual(ArrayBuffer);
+                // expect(axios.get).toHaveBeenCalledWith(
+                //     athlete_photo_url(photo_id)
+                // );
+            });
+        });
+        it('Returns an error', () => {
+            const photo_id = 15;
+            const errorMsg = 'default.error_occurred';
+            jest.spyOn(axios, 'get').mockImplementation(
+                () => Promise.reject(errorMsg)
+            );
+
+            return api.getAthletePicture().catch(e => {
+                expect(e).toBeInstanceOf(Error);
+                expect(e).toStrictEqual(Error(errorMsg));
+                // expect(axios.get).toHaveBeenCalledWith(
+                //     athlete_photo_url(photo_id)
+                // );
             });
         });
     });
