426b36789d36f0176286191f8dccd1aa420d27c0
Carlos Agullo <carlos.agullo@7shifts.com>
2020-04-18 04:05:53 -0400

    Abstracting actions

--- cypress/integration/olympics/baseapp_spec.js
+++ cypress/integration/olympics/baseapp_spec.js
@@ -3,55 +3,36 @@ describe("Open Cypress", () => {
     cy.visit("/")
     cy.contains("Olympic Athletes")
   })
-  it("should contain the list of games", () => {
-    cy.server()
 
-    const games = [
-      { city: "Tokyo", year: 2020 },
-      { city: "London", year: 2012 },
-      { city: "Pyeongchang", year: 2018 },
-      { city: "Vancouver", year: 2010 },
-      { city: "Beijing", year: 2008 },
-      { city: "Salt Lake City", year: 2002 },
-      { city: "Sydney", year: 2000 },
-      { city: "Nagano", year: 1998 },
-      { city: "Atlanta", year: 1996 },
-      { city: "Barcelona", year: 1992 },
-      { city: "Seoul", year: 1988 },
-      { city: "Los Angeles", year: 1984 },
-    ];
+  it("should contain the list of games", () => {
+    const games = Cypress.env('games')
 
     cy.visit("/")
-
+    cy.waitForMainPageToLoad()
+    
     games.forEach(function (game) {
-      const game_title = `${game.city} ${game.year}`;
-      cy.contains(game_title)
+      cy.verifyGameTitleIsPresent(game.city, game.year)
     })
   })
+
   it("should navigate to the detailed view of an athlete", () => {
-    const athlete_id = 4;
-    const games = "games_2016";
-    const games_with_medals = ["2016", "2012"];
-    cy.server()
+    const athlete_id = 4
+    const game_id = "games_2016"
+    const games_with_medals = ["2016", "2012"]
+    
     cy.route("GET", `athletes/${athlete_id}/results`).as("getAthleteResult")
+    
     cy.visit("/")
-    cy.get(`[data-testid="${games}"]`).within(() => {
-      cy.get(
-        `[data-testid="container_athlete_thumbnail_${athlete_id}"]`
-      ).click()
-    })
+    cy.waitForMainPageToLoad()
+    
+    cy.clickOnAthleteOnSelectedGame(game_id, athlete_id)
+
     cy.wait("@getAthleteResult")
       .then(() => {
         cy.url().should("include", `/athlete/${athlete_id}`)
       })
       .then(() => {
-        cy.get('[data-testid="arrow_back_from_athlete_detailed_card"]')
-        cy.get(`[data-testid="thumbnail_athlete_${athlete_id}"]`)
-        cy.get('[data-testid="athlete_name_surname"]')
-        cy.get('[data-testid="athlete_info_name"]');
-        cy.get('[data-testid="athlete_info_dob"]')
-        cy.get('[data-testid="athlete_info_weight"]')
-        cy.get('[data-testid="athlete_info_height"]')
+        cy.getAllPersonalInfoFromDetailedView(athlete_id)
       })
       .then(() => {
         cy.get('[data-testid="medals_container"]')
@@ -60,21 +41,19 @@ describe("Open Cypress", () => {
         })
       })
   })
+
   it("should be able to navigate back to the main page", () => {
     const athlete_id = 4
-    const games = "games_2016"
-    cy.server()
+    const game_id = "games_2016"
+    
     cy.route("GET", `athletes/${athlete_id}/results`).as("getAthleteResult")
-    cy.route("GET", '/games').as("getGamesPage")
+    
     cy.visit("/")
-    cy.get(`[data-testid="${games}"]`).within(() => {
-      cy.get(
-        `[data-testid="container_athlete_thumbnail_${athlete_id}"]`
-      ).click()
-    })
-    cy.wait("@getAthleteResult")
-      .its('status')
-      .should('be', 200)
+    cy.waitForMainPageToLoad()
+    
+    cy.clickOnAthleteOnSelectedGame(game_id, athlete_id)
+
+    cy.waitForAthleteResultRequestToComplete()
       .then(() => {
         cy.url().should("include", `/athlete/${athlete_id}`)
       })
@@ -82,9 +61,7 @@ describe("Open Cypress", () => {
         cy.get('[data-testid="arrow_back_from_athlete_detailed_card"]').click()
       })
       .then(() => {
-        cy.wait("@getGamesPage")
-          .its('status')
-          .should('be', 200)
+        cy.waitForMainPageToLoad()
           .then(() => {
             cy.url().should("eq", `${Cypress.env('baseUrl')}/`)
         })
